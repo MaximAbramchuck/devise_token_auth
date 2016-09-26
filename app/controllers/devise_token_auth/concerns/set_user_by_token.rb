5e3d0f0e0bed6fc4c04b0021ca8ed685b8edb6b9
Maxim Abramchuk <MaximAbramchuck@gmail.com>
2016-09-26 17:13:24 +0300

    Update set_user_by_token.rb

--- app/controllers/devise_token_auth/concerns/set_user_by_token.rb
+++ app/controllers/devise_token_auth/concerns/set_user_by_token.rb
@@ -18,20 +18,23 @@ module DeviseTokenAuth::Concerns::SetUserByToken
   # user auth
   def set_user_by_token(mapping=nil)
     # determine target authentication class
-    rc = resource_class(mapping)
+    rc_class = ->(m=nil) do
+      if m
+        Devise.mappings[m]
+      else
+        Devise.mappings[resource_name] || Devise.mappings.values.first
+      end.to
+    end
+
+    rc = resource_class(mapping) rescue rc_class.call(mapping)
 
     # no default user defined
-    return unless rc
-
-    #gets the headers names, which was set in the initialize file
-    uid_name = DeviseTokenAuth.headers_names[:'uid']
-    access_token_name = DeviseTokenAuth.headers_names[:'access-token']
-    client_name = DeviseTokenAuth.headers_names[:'client']
+    return nil unless rc
 
     # parse header for values necessary for authentication
-    uid        = request.headers[uid_name] || params[uid_name]
-    @token     ||= request.headers[access_token_name] || params[access_token_name]
-    @client_id ||= request.headers[client_name] || params[client_name]
+    uid        = request.headers['uid'] || params['uid']
+    @token     = request.headers['access-token'] || params['access-token']
+    @client_id = request.headers['client'] || params['client']
 
     # client_id isn't required, set to 'default' if absent
     @client_id ||= 'default'
@@ -52,21 +55,16 @@ module DeviseTokenAuth::Concerns::SetUserByToken
     # ensure we clear the client_id
     if !@token
       @client_id = nil
-      return
+      return nil
     end
 
-    return false unless @token
+    # return false unless @token
 
     # mitigate timing attacks by finding by uid instead of auth token
     user = uid && rc.find_by_uid(uid)
 
     if user && user.valid_token?(@token, @client_id)
-      # sign_in with bypass: true will be deprecated in the next version of Devise
-      if self.respond_to? :bypass_sign_in
-        bypass_sign_in(user, scope: :user)
-      else
-        sign_in(:user, user, store: false, bypass: true)
-      end
+      sign_in(:user, user, store: false, bypass: true)
       return @resource = user
     else
       # zero all values previously set values
@@ -84,10 +82,6 @@ module DeviseTokenAuth::Concerns::SetUserByToken
     @client_id = nil unless @used_auth_by_token
 
     if @used_auth_by_token and not DeviseTokenAuth.change_headers_on_each_request
-      # should not append auth header if @resource related token was
-      # cleared by sign out in the meantime
-      return if @resource.reload.tokens[@client_id].nil?
-
       auth_header = @resource.build_auth_header(@token, @client_id)
 
       # update the response header
@@ -98,9 +92,6 @@ module DeviseTokenAuth::Concerns::SetUserByToken
       # Lock the user record during any auth_header updates to ensure
       # we don't have write contention from multiple threads
       @resource.with_lock do
-        # should not append auth header if @resource related token was
-        # cleared by sign out in the meantime
-        return if @used_auth_by_token && @resource.tokens[@client_id].nil?
 
         # determine batch request status after request processing, in case
         # another processes has updated it during that processing
